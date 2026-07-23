64f3aef2698e935483e558762de4928f98c49b7b
rohan <rairohan14303@gmail.com>
2026-07-23 07:31:07 +0545

    Fixed some issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import PlaylistProvider from './context/PlaylistContext'
 import NavBar from './components/NavBar'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Playlist from './pages/Playlist'
+import Search from './pages/Search'
 import './App.css'
 
 function App() {
   return (
-    <>
-      <PlaylistProvider>
-        <NavBar/>
-      </PlaylistProvider>
-    </>
+    <PlaylistProvider>
+      <NavBar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/playlist' element={<Playlist/>}/>
+        <Route path='/search' element={<Search/>}/>
+      </Routes>
+    </PlaylistProvider>
   )
 }
 
