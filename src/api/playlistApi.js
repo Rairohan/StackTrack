c131c4d2bfff2b5c48a5505cf75be7c2fab62359
rohan <rairohan14303@gmail.com>
2026-08-07 11:24:50 +0545

    Api:tested and fixed

--- src/api/playlistApi.js
+++ src/api/playlistApi.js
@@ -1,5 +1,5 @@
 export async function searchTracks(term) {
     const response = await fetch(`https://itunes.apple.com/search?term=${encodeURIComponent(term)}&media=music&entity=song`)
-    const data = (await response).json()
+    const data = await response.json()
     return data.results
 }
