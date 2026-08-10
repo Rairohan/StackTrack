5c9ffdc3ae323afca9cb3bfffb1fb57ac0db2033
rohan <rairohan14303@gmail.com>
2026-08-10 21:15:41 +0545

    Updated Navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,9 +4,7 @@ export default function NavBar(){
     return(
         <div>          
             <Link to="/">Home</Link>
-            <Link to="/playlist">
-                My Playlist
-            </Link>
+            <Link to="/playlist"> My Playlist</Link>
             <SearchBar/>
         </div>
     )
