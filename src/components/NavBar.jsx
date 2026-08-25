397b9c0d2583ea13104da1cc73483a9ed6e4c576
rohan <rairohan14303@gmail.com>
2026-08-25 21:00:03 +0545

    Navbar tailwind added

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,10 +2,14 @@ import { Link } from 'react-router-dom';
 import SearchBar from './SearchBar';
 export default function NavBar(){
     return(
-        <div>          
-            <Link to="/">Home</Link>
-            <Link to="/playlist"> My Playlist</Link>
-            <SearchBar/>
+        <div className='bg-gray-800 text-white flex flex-col items-center p-5 '>
+            <div className='flex gap-5'>
+                <Link to="/" className='text-4xl '>Home</Link>
+                <Link to="/playlist" className='text-4xl '> My Playlist</Link>
+            </div>  
+            <div className='mt-4'>
+                <SearchBar/>  
+            </div>           
         </div>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,14 @@
 export default function Home() {
     return (
-        <h1>Home</h1>
+        <div className='text-center mt-8 text-4xl font-sans  text-gray-800'>
+             <h1>Hey!!!</h1>
+             <p className='mt-4 text-3xl  mx-auto max-w-5xl'>
+                Welcome to the StackTrack application! 
+                This is your go-to platform for managing and organizing your favorite playlists. 
+                Whether you're a music enthusiast or just looking to keep your tracks in order, StackTrack has got you covered. 
+                Explore the features, create your own playlists, and enjoy a seamless music experience.
+             </p>
+        </div>
+       
     )
 }
