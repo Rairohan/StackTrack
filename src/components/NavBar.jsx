68caab443a80a0ce2df04b26a7e7216f3f921aae
rohan <rairohan14303@gmail.com>
2026-08-26 12:35:04 +0545

    Home page tailwind updated

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,10 +2,10 @@ import { Link } from 'react-router-dom';
 import SearchBar from './SearchBar';
 export default function NavBar(){
     return(
-        <div className='bg-gray-800 text-white flex flex-col items-center p-5 '>
-            <div className='flex gap-5'>
-                <Link to="/" className='text-4xl '>Home</Link>
-                <Link to="/playlist" className='text-4xl '> My Playlist</Link>
+        <div className='bg-gray-800 text-white flex flex-col items-center p-7 '>
+            <div className='flex gap-10'>
+                <Link to="/" className='text-4xl hover:text-green-700 '>Home</Link>
+                <Link to="/playlist" className='text-4xl hover:text-green-700 '> My Playlist</Link>
             </div>  
             <div className='mt-4'>
                 <SearchBar/>  
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,15 @@
 export default function Home() {
     return (
-        <div className='text-center mt-8 text-4xl font-sans  text-gray-800'>
-             <h1>Hey!!!</h1>
-             <p className='mt-4 text-3xl  mx-auto max-w-5xl'>
-                Welcome to the StackTrack application! 
-                This is your go-to platform for managing and organizing your favorite playlists. 
-                Whether you're a music enthusiast or just looking to keep your tracks in order, StackTrack has got you covered. 
-                Explore the features, create your own playlists, and enjoy a seamless music experience.
-             </p>
+        <div className='text-center mt-8 font-serif text-white '>
+             <div className='bg-gray-400/30 p-9 mt-70 rounded-3xl mx-auto max-w-4xl hover:scale-105 transition-transform duration-300'>
+                <h1 className='text-5xl '>Hey!!!</h1>
+                <p className='mt-4 text-4xl  mx-auto max-w-6xl '>
+                     Welcome to the StackTrack application! 
+                     This is your go-to platform for managing and organizing your favorite playlists. 
+                     Whether you're a music enthusiast or just looking to keep your tracks in order, StackTrack has got you covered. 
+                     Explore the features, create your own playlists, and enjoy a seamless music experience.
+                </p>
+             </div>
         </div>
        
     )
